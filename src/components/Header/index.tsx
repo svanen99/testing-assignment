1f3a9183fb6a025cdd777d212bfbf318cb157d2b
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-08-28 09:21:10 +0200

    header styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,10 @@
-import sunflower from "../../../public/sunflower.jpg";
-
 const Header = () => {
   return (
-    <div className="flex flex-col bg-hero-image bg-cover bg-center text-center p-2 pt-20 sm:p-5 sm:h-72">
-      <h2 className="font-flowers text-4xl tracking-wider align-text-bottom text-right opacity-50 text-gray-300 sm:text-7xl">
+    <div className=" flex flex-col justify-between bg-hero-image bg-cover bg-left h-44 p-2 sm:p-5 sm:h-72">
+      <h2 className="font-flowers text-4xl text-right tracking-wider opacity-30 text-gray-300 sm:text-6xl">
         Let's bloom together
       </h2>
+      <h1 className="text-sm text-gray-300 uppercase font-SansNarrow drop-shadow-xl sm:text-2xl">ALEXANDRA BLOMBERG</h1>
     </div>
   );
 };
