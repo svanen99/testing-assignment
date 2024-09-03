c94c1860c5a8167617c4758b1230783f73b59d70
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-09-03 15:26:15 +0200

    make header sticky

--- src/components/ProjectSection/index.tsx
+++ src/components/ProjectSection/index.tsx
@@ -10,7 +10,7 @@ const ProjectSection = () => {
   
   return (
     <section datatest-id="project-section" className="md:p-14">
-      <h2 className="text-gray-200 text-center py-6 font-SansNarrow font-semi-bold text-lg md:text-2xl">My Projects</h2>
+      <h2 className="text-gray-200 bg-black text-center py-6 font-SansNarrow font-semi-bold text-lg md:text-2xl sticky top-0 z-10">My Projects</h2>
       <ProjectList projectItems={projects}/>
       {/* <ShowAllButton /> */}
     </section>
