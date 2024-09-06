37ae560f13925080e9cf8e11e07615347b9a555d
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-09-06 12:53:18 +0200

    pass state props to test components and create more mockdata necessary for tests

--- src/components/ProjectList/ProjectList.test.tsx
+++ src/components/ProjectList/ProjectList.test.tsx
@@ -1,21 +1,97 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import ProjectList from ".";
+import { ProjectType } from "@/utils/types";
+
+const mockProjects: ProjectType[] = [
+    {
+      id: "1",
+      title: "Project 1",
+      imgSrc: "/images/project1.png",
+      alt: "Snapshot of Project 1.",
+      description: "Description of Project 1.",
+      githubLink: "https://mock.github.com/project-1",
+      websiteLink: "https://project-1.com",
+      favourite: false
+    },
+    {
+      id: "2",
+      title: "Project 2",
+      imgSrc: "/images/project2.png",
+      alt: "Snapshot of Project 2.",
+      description: "Description of Project 2.",
+      githubLink: "https://mock.github.com/project-2",
+      websiteLink: "https://project-2.com",
+      favourite: false
+    },
+    {
+      id: "3",
+      title: "Project 3",
+      imgSrc: "/images/project3.png",
+      alt: "Snapshot of Project 3.",
+      description: "Description of Project 3.",
+      githubLink: "https://mock.github.com/project-3",
+      websiteLink: "https://project-3.com",
+      favourite: false
+    },
+    {
+      id: "4",
+      title: "Project 4",
+      imgSrc: "/images/project4.png",
+      alt: "Snapshot of Project 4.",
+      description: "Description of Project 4.",
+      githubLink: "https://mock.github.com/project-4",
+      websiteLink: "https://project-4.com",
+      favourite: false
+    },
+    {
+      id: "5",
+      title: "Project 5",
+      imgSrc: "/images/project5.png",
+      alt: "Snapshot of Project 5.",
+      description: "Description of Project 5. ",
+      githubLink: "https://mock.github.com/project-5",
+      websiteLink: "https://project-5.com",
+      favourite: false
+    },
+    {
+      id: "6",
+      title: "Project 6",
+      imgSrc: "/images/project6.png",
+      alt: "Snapshot of Project 6.",
+      description: "Description of Project 6.",
+      githubLink: "https://mock.github.com/project-6",
+      websiteLink: "https://project-6.com",
+      favourite: false
+    },
+    {
+      id: "7",
+      title: "Project 7",
+      imgSrc: "/images/project7.png",
+      alt: "Snapshot of Project 7.",
+      description: "Description of Project 7",
+      githubLink: "https://mock.github.com/project-7",
+      websiteLink: "https://project-7.com",
+      favourite: false
+    }
+  ];
+
+  const mockOnFavouriteToggle = () => {}
 
 describe("The ProjectList is rendered correctly", () => {
   it("Checks that the projectlist-container is rendered", () => {
-    render(<ProjectList />);
+    render(<ProjectList onFavouriteToggle={mockOnFavouriteToggle} projectItems={mockProjects} favouriteProjects={[]} />);
     const projectList = screen.getByTestId("project-list");
     expect(projectList).toBeInTheDocument();
   });
 
   it("Checks that the first four projects are rendered", () => {
-    render(<ProjectList />);
+    render(<ProjectList onFavouriteToggle={mockOnFavouriteToggle} projectItems={mockProjects} favouriteProjects={[]} />);
     const visibleProjects = screen.queryAllByTestId("project");
     expect(visibleProjects.length).toBe(4);
   });
 
-  it("Checks that the four visible projects has a specific title", () => {
-    render(<ProjectList />);
+  it("Checks that the four visible projects have a specific title", () => {
+    render(<ProjectList onFavouriteToggle={mockOnFavouriteToggle} projectItems={mockProjects} favouriteProjects={[]} />);
 
     const defaultProjects = screen.queryAllByTestId("project");
 
@@ -25,8 +101,14 @@ describe("The ProjectList is rendered correctly", () => {
     expect(defaultProjects[3]).toHaveTextContent("Project 4"); 
   });
 
+  it("Checks for showAll-button", () => {
+    render(<ProjectList onFavouriteToggle={mockOnFavouriteToggle} projectItems={mockProjects} favouriteProjects={[]} />);
+    const showAllButton = screen.getByTestId("show-all-btn");
+    expect(showAllButton).toBeInTheDocument();
+  })
+
   it("Checks control of showAll state when the ShowAllButton is clicked", () => {
-    render(<ProjectSection />);
+    render(<ProjectList onFavouriteToggle={mockOnFavouriteToggle} projectItems={mockProjects} favouriteProjects={[]} />);
     const visibleProjects = screen.getAllByTestId("project");
     expect(visibleProjects.length).toBe(4);
 
@@ -39,17 +121,5 @@ describe("The ProjectList is rendered correctly", () => {
     fireEvent.click(showAllButton);
     const visibleProjectsAfterReset = screen.getAllByTestId("project");
     expect(visibleProjectsAfterReset.length).toBe(4);
-  });
-
-  it("Checks that showAll state resets when the ShowAllButton is clicked again", () => {
-    render(<ProjectSection />);
-    const visibleProjects = screen.getAllByTestId("project");
-    expect(visibleProjects.length).toBe(4);
-
-    const showAllButton = screen.getByTestId("show-all-btn");
-    fireEvent.click(showAllButton);
-
-    const allVisibleProjects = screen.queryAllByTestId("project");
-    expect(allVisibleProjects.length).toBeGreaterThan(4);
   });
 });
