a701fcad352d5a3176aba29ec3e09d08fa59950f
Lisette Svan <svanlisette@gmail.com>
2024-09-16 17:23:58 +0200

    made branch for header, most done but forgot to switch branch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,11 +8,8 @@ const Header = () => {
             <div>
                 <h1>ALEXANDRA BLOMBERG</h1>
             </div>
-            {/* <Image src="/images/floating-sunflowers.png" alt="Sunflower picture"/> */}
         </div>
-        
         </>
     )
 }
-
 export default Header; 
