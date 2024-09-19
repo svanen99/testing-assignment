dbc375e4adf878aab1a25418caaead161343fe21
Lisette Svan <svanlisette@gmail.com>
2024-09-19 16:05:30 +0200

    done here for now

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,26 +1,28 @@
 import { AboutMeType } from "@/utils/types";
-import { section } from "framer-motion/client";
 
 const AboutMe = ({ start, experience, nextStep, purpose}: AboutMeType) => {
     return (
-        <section 
-            data-testid="about-me-section"
-        >
-            <h2>About Me</h2>
-            <h3>I´m Alexandra</h3>
-
-            <p data-testid="start">
-                {start}
-            </p>
-            <p data-testid="experience">
-                {experience}
-            </p>
-            <p data-testid="next-step">
-                {nextStep}
-            </p>
-            <p data-testid="purpose">
-                {purpose}
-            </p>
+        <section data-testid="about-me-section">
+            <div>
+                    <h2>
+                        About Me
+                    </h2>
+                    <h3>
+                        I'm Alexandra,
+                    </h3>
+                    <p data-testid="start">
+                        {start}
+                    </p>
+                    <p data-testid="experience">
+                        {experience}
+                    </p>
+                    <p data-testid="next-step">
+                        {nextStep}
+                    </p>
+                    <p data-testid="purpose">
+                        {purpose}
+                    </p>
+            </div>
         </section>
     )
 }
