e546750ebb6d8a19d313340bd4793987241cf7a4
Lisette Svan <svanlisette@gmail.com>
2024-09-19 12:24:15 +0200

    first commit, made a few test checks

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+    return (
+        <>
+            <div data-testid="footer">
+                <h3>Say Hello</h3>
+                <div>
+                    <a href="mailto:blombergalexandras@gmail.com">
+                        <span>blombergalexandras@gmail.com</span>
+                    </a>
+                    <a href="https://linkedin.com/in/alexandra-blomberg-7231a616a">
+                        <span>LinkedIn</span>
+                    </a>
+                </div>
+                    <p>Alexandra Blomberg &copy; &nbsp;</p>
+                    <a href="https://github.com/blombergalex/alex-bloom">
+                        Source code
+                    </a>
+
+            </div>
+        
+        </>
+    
+    )
+}
+
+export default Footer;
