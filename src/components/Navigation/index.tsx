322361038244ead69fb73b3257536a567c9caf27
Lisette Svan <svanlisette@gmail.com>
2024-09-20 14:12:49 +0200

    happy here for now styling left

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,32 @@
+// import { nav } from "framer-motion/client";
+// import { useState } from "react";
+
+
+// type NavigationProps = {
+//     onScrollToSection: (section: "aboutMe" | "projects" | "connect") => void;
+// };
+
+// const toggleMenu = () => {
+//     if(!menuOpen) {
+//         document.body.classList.add("overflow-hidden");
+//     } else {
+//         document.body.classList.remove("overflow-hidden")
+//     }
+//     setMenuOpen(!menuOpen);
+// }
+
+// const Navigation = () => {
+
+//     return(
+//         <nav>
+//             <div>
+//                 <button data-testid="open-menu-btn">
+//                     onClick={toggleMenu}
+//                 </button>
+//             </div>
+            
+//         </nav>
+//     )
+// }
+
+// export default Navigation;
