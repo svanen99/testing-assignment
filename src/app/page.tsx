5d9e847e8e8540d8949d097ccf9009da66902b56
Lisette Svan <svanlisette@gmail.com>
2024-09-21 15:55:46 +0200

    okay with styling for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import AboutMe from "@/components/About";
 import Connect from "@/components/Connect";
-import ProjectSection from "@/components/ProjectSection";
+// import ProjectSection from "@/components/ProjectSection";
 import { about, connect, projects  } from "@/utils/data";
 
 export default function Home() {
   
   return (
     <>
-      <AboutMe {...about}/>
-      <ProjectSection projects={projects} />
+      {/* <AboutMe {...about}/> */}
+      {/* <ProjectSection projects={projects} /> */}
       <Connect {...connect} />
     </>
   );
