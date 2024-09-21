bad56da36b9592c16921983609742f97f9f854b3
Lisette Svan <svanlisette@gmail.com>
2024-09-21 16:05:15 +0200

    forgot to add the spread operator in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <>
       <AboutMe {...about}/>
-      {/* <ProjectSection projects={projects} />
-      <Connect {...connect} /> */}
-      <Connect />
+      {/* { <ProjectSection projects={projects} /> */}
+      <Connect {...connect} /> 
+
     </>
   );
 }
