1f0170cea1be30f917067891e56ef2dfe762f5dd
Lisette Svan <svanlisette@gmail.com>
2024-09-21 17:32:01 +0200

    working on the design still

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import AboutMe from "@/components/About";
-import Connect from "@/components/Connect";
-import ProjectSection from "@/components/ProjectSection";
+// import Connect from "@/components/Connect";
+// import ProjectSection from "@/components/ProjectSection";
 import { about, connect, projects  } from "@/utils/data";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   
   return (
     <>
       <AboutMe {...about}/>
-      <ProjectSection projects={projects} />
-      <Connect {...connect} />
+      {/* <ProjectSection projects={projects} />
+      <Connect {...connect} /> */}
     </>
   );
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,23 @@
 const Footer = () => {
     return (
         <>
-            <div data-testid="footer">
-                <h3>Say Hello</h3>
-                <div>
-                    <a href="mailto:blombergalexandras@gmail.com">
-                        <span>blombergalexandras@gmail.com</span>
-                    </a>
-                    <a href="https://linkedin.com/in/alexandra-blomberg-7231a616a">
-                        <span>LinkedIn</span>
-                    </a>
-                </div>
-                    <p>Alexandra Blomberg &copy; &nbsp;</p>
-                    <a href="https://github.com/blombergalex/alex-bloom">
-                        Source code
-                    </a>
-
-            </div>
-        
+            <div data-testid="footer" className="text-white bg-amber-500 p-12 flex flex-col">
+                <h3 className="flex text-lg mb-5 md:text-xl">Say Hello</h3>
+                <div className="flex flex-col space-y-4 text-sm md:text-lg">
+          <a href="mailto:blombergalexandras@gmail.com"className="flex items-center space-x-2 hover:text-gray-200">
+            <span>blombergalexandras@gmail.com</span>
+          </a>
+          <a href="https://linkedin.com/in/alexandra-blomberg-7231a616a"className="flex items-center space-x-2 hover:text-gray-200 pb-10">
+            <span>LinkedIn</span>
+          </a>
+        </div>
+        <p data-testid="bottom-text" className="border-t-2 text-center pt-14">
+            Alexandra Blomberg &copy; | &nbsp;
+            <a href="https://github.com/blombergalex/alex-bloom">
+                Source code
+            </a>
+        </p>
+    </div>
         </>
     
     )
