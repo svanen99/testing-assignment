9aff5943054619f0cc8df75a86d0d1a12d1723bf
Lisette Svan <svanlisette@gmail.com>
2024-09-23 18:40:14 +0200

    saving for now

--- src/components/ProjectList/index.tsx
+++ src/components/ProjectList/index.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Project from "../Project";
-import { ProjectType } from "@/utils/types"
-import { useState } from "react"
+import { ProjectType } from "@/utils/types";
 
 type ProjectListProps = {
     projectItems: ProjectType[];
+    favouriteProjects: string[];
     onFavouriteToggle: (id: string) => void;
-    favouriteProjects: string[]
 }
 
-const ProjectList = ({ projectItems, onFavouriteToggle, favouriteProjects}: ProjectListProps ) => {
-    
-}
+const ProjectList = ({ projectItems, onFavouriteToggle, favouriteProjects }: ProjectListProps) => {
+  const [showAll, setShowAll] = useState(false);
+
+  return (
+    <div className="flex flex-col m-4 mx-auto">
+      <div
+        data-testid="project-list"
+        className={`flex flex-wrap text-gray-200 justify-center ${
+          projectItems.length % 2 ? "lg:justify-start" : ""
+        }`}
+      >
+        {(showAll ? projectItems : projectItems.slice(0, 4)).map((item) => (
+          <Project key={item.id}
+            {...item}
+            onFavouriteToggle={onFavouriteToggle}
+            isFavourite={favouriteProjects.includes(item.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default ProjectList;
