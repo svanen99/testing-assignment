21d4a95ed2d281203794aaf2a57fa488410a0719
Lisette Svan <svanlisette@gmail.com>
2024-09-25 15:26:03 +0200

    added picture

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 const Header = () => {
     return (
         <>
-        <div data-testid="header">
-            <h2 className="text-amber-800 opacity-55 p-4 text-3xl md:text-4xl text-right font-flowers mr-4 md:m-6">Welcome to my portfolio</h2>
-            <div>
+        <div data-testid="header" className="flex flex-col">
+            <h2 className="text-amber-800 opacity-55 p-4 text-3xl md:text-5xl text-right font-flowers mr-4 md:m-6">Welcome to my portfolio</h2>
+            <div className="flex justify-between items-end">
                 <h1 className="text-white font-SansNarrow ml-4 text-3xl md:ml-10 ">ALEXANDRA BLOMBERG</h1>
+                <Image className="justify-right w-auto md:h-58" src="/images/floating-sunflowers.png" alt="Sunflowers in header" width={340} height={225} />
             </div>
         </div>
         </>
