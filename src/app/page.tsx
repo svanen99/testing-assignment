3f199e8fd60fa74693684a7a8a2ef16dad05d659
Lisette Svan <svanlisette@gmail.com>
2024-09-26 16:36:15 +0200

    try to deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
+'use client'
 import AboutMe from "@/components/About";
 import Connect from "@/components/Connect";
-import { about, connect, projects  } from "@/utils/data";
+import { about, connect, projects } from "@/utils/data";
 import Navigation from "@/components/Navigation";
+import React, { useState } from "react";
+
 
 export default function Home() {
-  
+  const [activeSection, setActiveSection] = useState<"aboutMe" | "projects" | "connect">("aboutMe");
+
+  const handleScrollToSection = (section: "aboutMe" | "projects" | "connect") => {
+    setActiveSection(section);
+    const element = document.getElementById(section);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <>
-      <Navigation />
-      <AboutMe {...about}/>
-      <Connect {...connect} /> 
+      <Navigation onScrollToSection={handleScrollToSection} activeSection={activeSection} />
+      <AboutMe {...about} />
+      <Connect {...connect} />
     </>
   );
 }
